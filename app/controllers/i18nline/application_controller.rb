e0240c176220375355807bce7ab23ed6763f6133
Chipairon <chipairon@fake.server>
2013-11-18 17:58:05 +0100

    added config options, memoization and authorization for translations

--- app/controllers/i18nline/application_controller.rb
+++ app/controllers/i18nline/application_controller.rb
@@ -1,4 +1,13 @@
 module I18nline
   class ApplicationController < ActionController::Base
+    before_action :check_authenticated_user
+
+    private
+
+    def check_authenticated_user
+      unless(I18nline.current_user and I18nline.current_user.try(I18nline.can_translate_method))
+        redirect_to I18nline::login_route, error: "You need to login first"
+      end
+    end
   end
 end
--- lib/i18nline.rb
+++ lib/i18nline.rb
@@ -2,4 +2,27 @@ require "i18nline/engine"
 require 'i18nline/railtie' if defined?(Rails)
 
 module I18nline
+
+  ## CONFIGURATION OPTIONS:
+
+  mattr_accessor :current_user_method
+  @@current_user_method = "current_user"
+
+  mattr_accessor :can_translate_method
+  @@can_translate_method = "can_translate?"
+
+  mattr_accessor :login_route
+  @@login_route = "/login"
+
+  mattr_accessor :enabled_locales
+  @@enabled_locales = %w(en es it)
+
+  def self.setup
+    yield self
+  end
+
+  private
+  mattr_accessor :current_user
+  @@current_user ||= nil
+
 end
