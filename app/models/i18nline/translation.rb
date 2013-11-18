e4911e1d44bd84d575eaea1b08ad89d705baf6cf
Chipairon <chipairon@fake.server>
2013-11-18 22:43:49 +0100

    Inline translation view. Prettify views. Keep track of locale on dummy app

--- app/models/i18nline/translation.rb
+++ app/models/i18nline/translation.rb
@@ -1,6 +1,10 @@
 module I18nline
   class Translation < ActiveRecord::Base
     after_save :update_caches
+    attr_accessor :make_nil
+    serialize :value
+    serialize :interpolations, Array
+
     def update_caches
       TRANSLATION_STORE.reload!
     end
--- test/dummy/app/controllers/application_controller.rb
+++ test/dummy/app/controllers/application_controller.rb
@@ -3,6 +3,16 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
+  before_action :set_locale
+
+  def set_locale
+    I18n.locale = params[:locale] || session[:locale] || locale_from_browser || default_language
+    if I18n.locale.to_s != session[:locale].to_s
+      logger.debug "Locale changed from -#{session[:locale].to_s}- to -#{I18n.locale.to_s}-."
+    end
+    session[:locale] = I18n.locale.to_s
+  end
+
   require 'ostruct'
   def current_user
     @current_user ||= OpenStruct.new(name: "John Smith", can_translate?: true)
