6ec3854981796185f5e6e165cee9f6fc61890d48
Chipairon <chipairon@fake.server>
2013-11-19 01:10:39 +0100

    added filters with scopes to index page. Removed debugger dependency

--- app/models/i18nline/translation.rb
+++ app/models/i18nline/translation.rb
@@ -5,6 +5,49 @@ module I18nline
     serialize :value
     serialize :interpolations, Array
 
+    default_scope { order("created_at desc") }
+
+    def self.not_translated(apply_this = "1")
+      if apply_this.present?
+        where("value is null")
+      else
+        scoped
+      end
+    end
+
+    def self.blank_value(apply_this = "1")
+      if apply_this.present?
+        #value is serialized so searching for empty is complicated:
+        where("value like ?", "".to_yaml)
+      else
+        scoped
+      end
+    end
+
+    def self.in_locale(locale)
+      if locale.present?
+        where("locale = ?", locale)
+      else
+        scoped
+      end
+    end
+
+    def self.search_key(to_search)
+      if to_search.present?
+        where("key like ?", "%#{to_search}%")
+      else
+        scoped
+      end
+    end
+
+    def self.search_value(to_search)
+      if to_search.present?
+        where("value like ?", "%#{to_search}%")
+      else
+        scoped
+      end
+    end
+
     def update_caches
       TRANSLATION_STORE.reload!
     end
