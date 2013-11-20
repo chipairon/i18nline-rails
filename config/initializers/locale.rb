5fd2a8d3e3e9b84492dea2a66168284ca3d0abc8
Chipairon <chipairon@fake.server>
2013-11-20 11:29:29 +0100

    update i18n-active_record version. Change scope of the patch to 'available_locales' to prevent its execution in chain.rb.

--- config/initializers/locale.rb
+++ config/initializers/locale.rb
@@ -4,10 +4,6 @@ module I18nline
     Translation.table_name = "i18nline_translations"
     include I18n::Backend::ActiveRecord::Missing
     include I18n::Backend::Memoize
-
-    def available_locales
-      I18nline::enabled_locales
-    end
   end
 end
 
@@ -15,8 +11,19 @@ end
 # rake when there are other gems using I18n at setup before the
 # table is created.
 if ActiveRecord::Base.connection.table_exists? 'i18nline_translations'
+  module I18n
+    module Backend
+      class Chain
+        def available_locales
+          I18nline::enabled_locales
+        end
+      end
+    end
+  end
+
   TRANSLATION_STORE = I18nline::MyBackend.new
   I18n.backend = I18n::Backend::Chain.new(TRANSLATION_STORE, I18n::Backend::Simple.new)
+
   module I18n
     class JustRaiseExceptionHandler < ExceptionHandler
       def call(exception, locale, key, options)
