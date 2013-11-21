834d681b8b34b51495a106029328736ae6c8dfcb
Chipairon <chipairon@fake.server>
2013-11-21 19:47:23 +0100

    precompile assets so it works in production.

--- lib/i18nline/railtie.rb
+++ lib/i18nline/railtie.rb
@@ -1,7 +1,8 @@
 require File.join(File.dirname(__FILE__), 'action_view_extension')
 module I18nline
   class Railtie < Rails::Railtie
-    initializer "i18nline.configure_rails_initialization" do
+    initializer "i18nline.configure_rails_initialization" do |app|
+      app.config.assets.precompile += %w(i18nline_to_host.js i18nline_to_host.css)
       ActiveSupport.on_load(:action_view) do
         ::ActionView::Base.send :include, I18nline::ActionViewExtension
       end
