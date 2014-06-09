20f792c2220886efb7793425a0e1d83132c52fee
Chipairon <chipairon@fake.server>
2014-06-10 00:31:20 +0200

    the core translation should be a task of the i18n-active_record gem. More fixes have been done there.

--- config/initializers/locale.rb
+++ config/initializers/locale.rb
@@ -20,50 +20,6 @@ begin
           def available_locales
             I18nline::enabled_locales
           end
-
-          module Implementation
-
-            # This is the entry point to make the translation.
-            # We need to override this method to store missing translations:
-            # 1st backend is AR, if it misses then we want to store the translation
-            # as missed, but if there is a default value or it is translated in other
-            # backend, we want to store that value, instead of nil.
-            def translate(locale, key, default_options = {})
-              namespace = nil
-              options = default_options.except(:default)
-              final_translation = nil
-              should_store_in_db = false
-
-              backends.each do |backend|
-                catched_value = catch(:exception) do
-                  options = default_options if backend == backends.last
-                  translation = backend.translate(locale, key, options)
-                  if namespace_lookup?(translation, options)
-                    namespace = translation.merge(namespace || {})
-                  elsif !translation.nil?
-                    final_translation = translation
-                    break
-                  end
-                end
-                if catched_value.class == MissingTranslation
-                  should_store_in_db = true
-                end
-              end
-
-              if final_translation.nil?
-                final_translation = namespace
-              end
-
-              if should_store_in_db
-                default_options[:default] = final_translation
-                TRANSLATION_STORE.store_default_translations(locale, key, default_options)
-              end
-
-              return final_translation if final_translation
-              throw(:exception, I18n::MissingTranslation.new(locale, key, options))
-            end
-          end
-
         end
       end
     end
