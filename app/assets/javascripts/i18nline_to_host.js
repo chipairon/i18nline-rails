a39517d00fbf58a7e5fcd12329aa66ac8960eb47
Chipairon <chipairon@fake.server>
2014-06-12 02:39:52 +0200

    inline translations popup was being fired twice.

--- app/assets/javascripts/i18nline_to_host.js
+++ app/assets/javascripts/i18nline_to_host.js
@@ -1,12 +1,14 @@
 $(document).ready(function() {
-  $('span.translation_missing').contextmenu(function(event) {
-    key = $(this).attr("title")
+  $('span.translation_missing').off("contextmenu").on("contextmenu", function(event) {
+    event.preventDefault();
+    event.stopPropagation();
+    key = $(this).attr("title");
     window.open("/i18nline/find_by_key?key=" + key, "_blank");
-    return false; //prevent context menu to show
   });
-  $('span.translation_found').contextmenu(function(event) {
-    key = $(this).attr("title")
+  $('span.translation_found').off("contextmenu").on("contextmenu", function(event) {
+    event.preventDefault();
+    event.stopPropagation();
+    key = $(this).attr("title");
     window.open("/i18nline/find_by_key?key=" + key, "_blank");
-    return false; //prevent context menu to show
   });
 });
